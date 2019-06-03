ffced65a04d89031c8713e603971537230e579ee
Jordan Morris <jthecybertinkerer@gmail.com>
2019-06-03 15:22:56 -0500

    Updated About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,6 +4,7 @@ import Card from "../Card/"
 import GithubLogo from "../../assets/img/GitHub_Logo.png"
 const About = (props) =>{
     return <Card id='About'>
+        <a id="About"></a>
         <h1>Jordan Morris</h1>
         {/* add padding after the name */}
         <div className="subheading">
@@ -12,7 +13,7 @@ const About = (props) =>{
         </div>
         <div id="profile-links">
             <div id="github-profile" className='profile-link'>
-                <img src={GithubLogo} alt='github-logo' />
+                <a href="https://www.github.com/jtmorrisbytes"><img src={GithubLogo} alt='github-logo' /></a>
             </div>
             <div id="linkedin-profile" className='profile-link'>
                 <p className='link-text'></p>
