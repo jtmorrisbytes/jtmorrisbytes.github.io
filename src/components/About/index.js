5696f13e84b4b52005df100b80bd21a2d34dea1c
Jordan Morris <jthecybertinkerer@gmail.com>
2019-06-05 23:58:30 -0500

    Completed html template, added styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,21 +4,17 @@ import Card from "../Card/"
 import GithubLogo from "../../assets/img/GitHub_Logo.png"
 const About = (props) =>{
     return <Card id='About'>
-        <a id="About"></a>
-        <h1>Jordan Morris</h1>
-        {/* add padding after the name */}
-        <div className="subheading">
-            <h2>Full Stack</h2>
-            <h3>Web Developer</h3>
-        </div>
-        <div id="profile-links">
-            <div id="github-profile" className='profile-link'>
-                <a href="https://www.github.com/jtmorrisbytes"><img src={GithubLogo} alt='github-logo' /></a>
+            <a id="About"></a>
+            <h1 id='name'>Jordan Morris</h1>
+            {/* add padding after the name */}
+            <div className="subheading">
+                <h2 id='job-scope'>Full Stack</h2>
+                <h3 id='job-title'>Web Developer</h3>
             </div>
-            <div id="linkedin-profile" className='profile-link'>
-                <p className='link-text'></p>
+            <div id="profile-links">
+                <a id='github-profile' href="https://www.github.com/jtmorrisbytes"><img src={GithubLogo} alt='github-logo' /></a>
+                <a id='linkedin-profile' href='https://www.linkedin.com/in/jtmorrisbytes'>LinkedIn</a>
             </div>
-        </div>
         
     </Card>
 }
