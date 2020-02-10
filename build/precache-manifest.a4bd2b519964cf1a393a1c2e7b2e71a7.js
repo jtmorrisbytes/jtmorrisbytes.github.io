118316c15056f9bed1ebee7a3eef681f60a3d618
Jordan Morris <jthecybertinkerer@gmail.com>
2020-02-10 01:22:43 -0600

    add latest build

---
+++ build/precache-manifest.a4bd2b519964cf1a393a1c2e7b2e71a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382dcb7891aa83e98ec13574955f98f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1437f0afadeecb0282",
+    "url": "/static/css/main.40a7a2c8.chunk.css"
+  },
+  {
+    "revision": "8a705d2810a898ac3678",
+    "url": "/static/js/2.845c00ac.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.845c00ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1437f0afadeecb0282",
+    "url": "/static/js/main.cb4b63dc.chunk.js"
+  },
+  {
+    "revision": "ff425bfadf5e2405c52e",
+    "url": "/static/js/runtime-main.33b94726.js"
+  },
+  {
+    "revision": "0208a1a28e4ec5338421a9f62b85e257",
+    "url": "/static/media/3983DC_0_0.0208a1a2.eot"
+  },
+  {
+    "revision": "3006c1ab687a7f3e01af6286ab3dca22",
+    "url": "/static/media/3983DC_0_0.3006c1ab.woff"
+  },
+  {
+    "revision": "8cb5acce54a3190ec12252638dfb004b",
+    "url": "/static/media/3983DC_0_0.8cb5acce.ttf"
+  },
+  {
+    "revision": "aac3446639ce058451b53a3e89b6db29",
+    "url": "/static/media/3983DC_0_0.aac34466.woff2"
+  }
+]);
