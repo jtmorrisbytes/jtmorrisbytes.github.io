38cbf87e7f13546bfb60fc2ec985ee0947e23967
Jordan Morris <jthecybertinkerer@gmail.com>
2020-02-13 01:54:09 -0600

    update app to have responsive fonts

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ const githubWorker = new Worker('githubApiWorker.js')
 
 window.addEventListener('unload',()=>{console.log("terminating worker");githubWorker.terminate(); window.githubWorker = undefined;})
 window.githubWorker = githubWorker
+// keep a DOM Reference to the root component
+const appRoot = document.getElementById('root')
+// enable interactive font growth on resize
+// window.addEventListener("onresize", (function dynamicFontSize(){
 
-ReactDOM.render(<App githubWorker={githubWorker}/>, document.getElementById('root'));
+// }).bind(this))
+
+ReactDOM.render(<App githubWorker={githubWorker}/>, appRoot);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
