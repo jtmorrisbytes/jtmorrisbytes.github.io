cba1f2fab1eac05e6c3c1b0107317c05ea90af88
Jordan Morris <jtmorrisbytes@gmail.com>
2020-11-04 17:37:39 -0600

    pruned unneccessary deps

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -20,19 +20,20 @@ class Nav extends React.Component {
             <p className="logo-text">Jordan Morris</p>
           </div>
           {/* <div id="nav-links"> */}
-          <a id="projects" href="#Projects">
+          <a id="projects" href="#to-projects">
             Projects
           </a>
-          <a id="about" href="#About">
+          <a id="about" href="#to-about">
             About
           </a>
 
-          <a id="contact" href="#Contact">
+          <a id="contact" href="#to-contact">
             Contact
           </a>
           <a
             id="resume"
             href="https://docs.google.com/document/d/1mZWsT0AtRXaKzSc4xQb2AX0i0Dh22MHvXrDRbqjDgSY/edit?usp=sharing"
+            target="_blank"
           >
             Resume
           </a>
